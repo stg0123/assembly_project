94633074d617767b6e172a65c961f3b2abcb7a26
denhur62 <63595768+denhur62@users.noreply.github.com>
2021-05-14 03:30:24 +0900

    modified login , accounts
    modified login , accounts

--- server/assembly/serializers.py
+++ server/assembly/serializers.py
@@ -5,4 +5,4 @@ from .models import AuthUser
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuthUser
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'username', 'password','is_superuser','is_staff','is_active','date_joined']
