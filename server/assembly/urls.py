40dabafe2e2249aba4c9c5c4c448111dddeaa90c
taegyun <sontg123@naver.com>
2021-07-18 16:53:35 +0900

    urls_update

--- server/assembly/urls.py
+++ server/assembly/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import *
 from django.conf.urls import include
-
-from .views import LawViewset, Top3Viewset, LawmakerViewset
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
